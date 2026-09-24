249640c7b4ce44b849d6fbe70ee61f799f18d995
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2026-09-24 13:18:38 +0300

    Каркас: Lab1, Task1.Downloader і тести

---
+++ Lab1/Task1/Task1.Downloader.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1.Downloader.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Lab1/Task1/Task1.Downloader/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Task1.Downloader;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
